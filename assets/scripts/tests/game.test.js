1c92b1e20275380966c8c0c8c9e0f2470dd65e56
Robyn Smith <robyn.roybin@gmail.com>
2023-01-12 17:38:12 +0000

    Add game object and tests

---
+++ assets/scripts/tests/game.test.js
@@ -0,0 +1,28 @@
+const { game } = require("../game");
+const { beforeAll, expect } = require("@jest/globals");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open(); 
+    document.write(fileContents); 
+    document.close();  
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true); 
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true); 
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true); 
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true); 
+    });
+    test("choices contains correct IDs", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+})
